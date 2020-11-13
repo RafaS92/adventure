47d785562ff1ee9c74d915d004b21aa982510e2d
RafaS92 <manrvaldez..92@gmail.com>
2020-11-13 13:33:51 -0600

    cards created

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import "./Cards.css"
 
-function CardItem() {
+function CardItem({path,label,src,text}) {
     return (
         <>
         <li className="cards__item">
-            <Link className="cards__item__link">
-                <figure className="cars__item_pic-wrap">
-                    <img src="/" alt="Travel image" className="cards__item__image"/>
+            <Link className="cards__item__link" to={path}>
+                <figure className="cards__item__pic-wrap" data-category={label}>
+                    <img src={src} alt="Travel image" className="cards__item__img"/>
                 </figure>
 
                 <div className="cards__item__info">
                     <h5 className="cards__item__text">
-                        
+                        {text}
                     </h5>
                 </div>
             </Link>
