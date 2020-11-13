c4fe1536510f3d635f1384df1469e8ada77ad692
RafaS92 <manrvaldez..92@gmail.com>
2020-11-13 16:47:35 -0600

    footer css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Button } from "./Button";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <div className="footer-container">
-      <section className="footer-subscription">
+      <section className="footer-subscription ">
         <p className="footer-subscription-heading">
           Join the adventure newsletter to receive our best vacation deals
         </p>
@@ -24,6 +25,38 @@ function Footer() {
           </form>
         </div>
       </section>
+      <div className="footer-link">
+        <div className="footer-links-wrapper">
+          <div className="footer-link-items">
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/sign-up">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/sign-up">Terms of Service</Link>
+          </div>
+          <div className="footer-link-items">
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/sign-up">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/sign-up">Terms of Service</Link>
+          </div>
+          <div className="footer-link-items">
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/sign-up">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/sign-up">Terms of Service</Link>
+          </div>
+          <div className="footer-link-items">
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/sign-up">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/sign-up">Terms of Service</Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
